7ddb8a6ed7e280f1503a60dfc16f88c9a71d0b1e
人民的勤务员 <china.qinwuyuan@gmail.com>
2024-10-22 04:14:31 +0800

    fix: 修复 TextFormatter 模块中的错误 共同提交者上面加上换行

--- _module_/TextFormatter.psm1
+++ _module_/TextFormatter.psm1
@@ -2,32 +2,21 @@
 	param(
 		[string]$filePath
 	)
-
-	# Read the content from the specified file
 	$content = Get-Content -Path $filePath
 	$content = $content | Where-Object { $_.Trim() -ne "" }
-	#$lineCount = $content.Count
-
 	if ($content.Count -eq 1) {
-		#Write-Host "文本内容只有一行,我要直接返回了: $lineCount"
 		return $content -join "`n"
 	}
-	# Skip the first line for backtick replacement, process the rest
 	$firstLine = $content[0]
 	$restOfContent = $content[1..($content.Length - 1)] -replace '`(.*?)`','[$1]'
-
 	# Combine the first line with the processed content
 	$content = @($firstLine) + $restOfContent
-
 	# Trim and filter out empty lines
 	$content = $content | ForEach-Object { $_.TrimStart() }
-
-
 	# Remove specific symbols
 	$content = $content -replace "\*\*","" # Remove "**"
 	$content = $content -replace "###","" # Remove "###"
 	$content = $content -replace "•","" # Remove "•"
-
 	# Add "+ " in front of all lines except the first one
 	$content = $content | ForEach-Object {
 		if ($_.Equals($content[0])) {
@@ -35,19 +24,17 @@
 		}
 		else {
 			if ($_ -like "*Co-authored-by*") {
-				$_ # 如果行包含 "Co-authored-by"，则不添加前缀
+				"`n" + $_ # 如果行包含 "Co-authored-by"，则不添加前缀
 			}
 			else {
 				"* " + $_ # 否则在前面添加 "* "
 			}
 		}
 	}
-
 	# Check if the first line starts with whitespace and trim it
 	if ($content[0].Length -gt 0 -and [char]::IsWhiteSpace($content[0][0])) {
 		$content[0] = $content[0].Substring(1)
 	}
-
 	# Return the formatted text
 	return $content -join "`n"
 }
