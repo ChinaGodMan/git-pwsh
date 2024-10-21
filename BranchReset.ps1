a014e7b2578619e9c63092d242b5e56d1d33d3e2
人民的勤务员 <china.qinwuyuan@gmail.com>
2024-10-22 04:17:23 +0800

    feature: 快捷重置分支脚本

---
+++ BranchReset.ps1
@@ -0,0 +1,19 @@
+﻿$commitHash = Read-Host "请输入提交哈希"
+$choice = Read-Host "请输入操作选项：0 (保留更改并回退到指定提交)、1 (丢弃更改并回退到指定提交)、2 (回退到上一个提交)"
+switch ($choice) {
+	0 {
+		git reset --soft $commitHash
+		Write-Host "已执行: git reset --soft $commitHash"
+	}
+	1 {
+		git reset --hard $commitHash
+		Write-Host "已执行: git reset --hard $commitHash"
+	}
+	2 {
+		git reset --hard HEAD
+		Write-Host "已执行: git reset --hard HEAD"
+	}
+	default {
+		Write-Host "无效的选项，请输入 0、1 或 2。"
+	}
+}
