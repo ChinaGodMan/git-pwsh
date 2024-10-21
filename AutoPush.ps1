c7086f93a5093496e35cf9b364877a840eab8dfe
人民的勤务员 <china.qinwuyuan@gmail.com>
2024-10-21 09:45:49 +0800

    feat: :octocat: 将`AutoPush`改为检测md5,针对不同的项目生成`.messages`文件

--- AutoPush.ps1
+++ AutoPush.ps1
@@ -1,78 +1,83 @@
-param (
-    [string]$messageFilePath,
-    [string]$customMessage
+﻿param(
+	[string]$messageFilePath,
+	[string]$customMessage
 )
-$modulePath = Join-Path -Path (Split-Path -Parent $MyInvocation.MyCommand.Path) -ChildPath "TextFormatter.psm1"
-Import-Module $modulePath -Force
-$nousefile = $false 
+#导入模块
+$modulePath = Join-Path -Path (Split-Path -Parent $MyInvocation.MyCommand.Path) -ChildPath "_module_"
+$moduleFiles = Get-ChildItem -Path $modulePath -Filter *.psm1
+foreach ($module in $moduleFiles) {
+	Import-Module -Name $module.FullName -Force -ErrorAction Stop
+}
+#是否在此次流程中使用文件内容
+$nousefile = $false
+$default = ":octocat: 更新文件"
 $scriptPath = $PSScriptRoot
 $rootDrive = [System.IO.Path]::GetPathRoot($scriptPath).TrimEnd('\')
-$green = "`e[32m"
-$reset = "`e[0m"
+#检查md5是否有变动
 $hasChanges = git status --porcelain | Where-Object { $_ -ne "" }
 if (-not $hasChanges) {
-    Write-Host "$green 没有检测到更改 $reset"
-    exit
+	Write-Host "`e[32m 没有检测到更改 `e[0m"
+	exit
 }
-if (-not $messageFilePath) {
-    $messageFilePath = "$rootDrive\1.txt"
-}
-$keyID = "88888888888888888" 
-$privateKeyPath = "$rootDrive\GitHub\88888888888888888.asc"
+$keyID = "88888888888888888"
+$privateKeyPath = "$rootDrive\GitHub\C111111111111111n-私钥.asc"
 $secretKey = gpg2 --list-secret-keys $keyID 2>&1
-if ($secretKey -like "*@gmail.com*") {
-    #Write-Host "私钥 $keyID 已存在。"
+if ($secretKey -like "*china.12223324@gmail.com*") {
+	#Write-Host "私钥 $keyID 已存在。"
 }
 else {
-    Write-Host "私钥 $keyID 不存在，正在导入..."
-    gpg2 --import $privateKeyPath
+	gpg2 --import $privateKeyPath
 }
-$oldCommitFilePath = "$scriptPath\oldcommitMessage.txt"
+# 设置文件路径
+if (-not $messageFilePath) {
+	$messageFilePath = (Get-Location).Path + "\.messages"
+}
 if (-not (Test-Path $messageFilePath)) {
-    Write-Error "提交信息文件不存在: $messageFilePath"
-    exit 1
-}
-$newCommitMessage = Get-Content $messageFilePath -Raw
-$oldCommitMessage = if (Test-Path $oldCommitFilePath) {
-    Get-Content $oldCommitFilePath -Raw
-}
-else {
-    "更新"
+	$default | Out-File -FilePath $messageFilePath -Encoding UTF8
 }
 $currentDateTime = Get-Date -Format "yyyy/M/d HH:mm:ss"
-if ($newCommitMessage -eq $oldCommitMessage) {
-    $commitMessage = "更新"
-    $nousefile = $true
+$result = Set-FileMd5Record -FilePath $messageFilePath
+if ($result -eq $true) {
+	<#     #NOTE - 保留 [内容变化 ]
+	$newCommitMessage = Get-Content $messageFilePath -Raw
+	$commitMessage = $newCommitMessage -replace '\$currentDateTime',$currentDateTime #>
+} elseif ($result -eq $false) {
+	#内容无变化
+	$commitMessage = $default
+	$nousefile = $true
 }
-else {
-    $commitMessage = $newCommitMessage -replace '\$currentDateTime', $currentDateTime
+if ($customMessage -ne "") { #使用这个自定义信息.
+	$nousefile = $true
+	$commitMessage = $customMessage -replace '\{Time\}',$currentDateTime
 }
-if ($customMessage -ne "") {
-    $nousefile = $true
-    $commitMessage = $customMessage -replace '\{Time\}', $currentDateTime
+$currentBranch = git rev-parse --abbrev-ref HEAD
+if ($LASTEXITCODE -ne 0) {
+	Write-Error "无法获取当前分支名称"
+	exit 1
+}
+if (-not $nousefile) {
+	# 使用文件内容构建提交消息
+	$commitMessage = Format-TextContent -FilePath $messageFilePath
+}
+# 兜底检查
+if ($commitMessage -eq "") {
+	$commitMessage = $default
 }
 Write-Host "当前分支: $currentBranch" -ForegroundColor Cyan
 Write-Host "使用的提交信息: $commitMessage" -ForegroundColor Cyan
-$currentBranch = git rev-parse --abbrev-ref HEAD
-if ($LASTEXITCODE -ne 0) {
-    Write-Error "无法获取当前分支名称"
-    exit 1
-}
-if (-not $nousefile) {
-    $commitMessage = Format-TextContent -filePath $messageFilePath
-}
+#ANCHOR - 推送
 git add .
 git commit -m $commitMessage
 if ($LASTEXITCODE -ne 0) {
-    Write-Error "提交失败"
-    exit 1
+	Write-Error "提交失败"
+	Set-FileRecord -FilePath $messageFilePath
+	exit 1
 }
-# 推送到当前
 Write-Host "推送到远程仓库的分支 $currentBranch..."
 git push origin $currentBranch --force
 if ($LASTEXITCODE -ne 0) {
-    Write-Error "推送失败"
-    exit 1
+	Write-Error "推送失败"
+	Set-FileRecord -FilePath $messageFilePath
+	exit 1
 }
 Write-Host "推送GitHub已完成!"
-Copy-Item -Path $messageFilePath -Destination $oldCommitFilePath -Force
---
+++ _module_/md5.psm1
@@ -0,0 +1,53 @@
+﻿function Set-FileMd5Record {
+	param(
+		[string]$filePath
+	)
+	$jsonFilePath = Join-Path -Path $PSScriptRoot -ChildPath ".MeseagesFilesMd5.json"
+	if (-not (Test-Path $filePath)) {
+		Write-Error "文件不存在: $filePath"
+		return
+	}
+	#!SECTION 获取md5
+	$fileMd5 = (Get-FileHash -Path $filePath -Algorithm MD5).Hash
+	# 检查 JSON 文件是否存在，如果不存在则创建空的内容
+	if (Test-Path $jsonFilePath) {
+		# NOTE -  JSON内容并转换为哈希表
+		$jsonContent = Get-Content -Path $jsonFilePath -Raw | ConvertFrom-Json
+		$hashTable = @{}
+		foreach ($key in $jsonContent.PSObject.Properties.Name) {
+			$hashTable[$key] = $jsonContent.$key
+		}
+	} else {
+		$hashTable = @{}
+		Write-Host "JSON 文件不存在，创建新的文件。"
+	}
+	$parentDirName = $filePath.Replace(":","").Replace("\","/")
+	# 检查哈希表中是否已经有该目录的记录
+	if ($hashTable.ContainsKey($parentDirName)) {
+		if ($hashTable[$parentDirName] -eq $fileMd5) {
+			return $false #md5未改变
+		} else { #md5改变
+			$hashTable[$parentDirName] = $fileMd5
+		}
+	} else {
+		$hashTable[$parentDirName] = $fileMd5
+	}
+	$hashTable | ConvertTo-Json -Depth 100 | Set-Content -Path $jsonFilePath -Force
+	return $true
+}
+function Set-FileRecord {
+	param(
+		[string]$filePath
+	)
+	$jsonFilePath = Join-Path -Path $PSScriptRoot -ChildPath ".MeseagesFilesMd5.json"
+	$parentDirName = $filePath.Replace(":","").Replace("\","/")
+	$jsonContent = Get-Content -Path $jsonFilePath -Raw | ConvertFrom-Json
+	$hashTable = @{}
+	foreach ($key in $jsonContent.PSObject.Properties.Name) {
+		$hashTable[$key] = $jsonContent.$key
+	}
+	$hashTable[$parentDirName] = "11111"
+	$hashTable | ConvertTo-Json -Depth 100 | Set-Content -Path $jsonFilePath -Force
+
+}
+
